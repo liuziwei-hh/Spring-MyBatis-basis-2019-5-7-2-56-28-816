abac981fdb0d1195f7958a01835fe48e0a4c0935
LZW <1312299240@qq.com>
2019-08-09 16:08:19 +0800

    m

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,37 +1,32 @@
-package com.example.demo.controller;
-
-import com.example.demo.dao.UserMapper;
-import com.example.demo.entity.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("")
-public class UserController {
-    @Autowired
-    private UserMapper userMapper;
-
-    @GetMapping("/user")
-    public List<User> findAll() {
-        return null;
-    }
-
-    @GetMapping("/user/{userId}")
-    public User findById(@PathVariable long userId) {
-        return null;
-    }
-
-    @PostMapping("/user")
-    public void insertUser(@RequestBody User user) {
-    }
-
-    @PutMapping("/user/{userId}")
-    public void updateUser(@RequestBody User user,@PathVariable int userId) {
-    }
-
-    @DeleteMapping("/user/{userId}")
-    public void deleteById(@PathVariable long userId) {
-    }
-}
+package com.example.demo.controller; 
+   import com.example.demo.dao.UserMapper; 
+ import com.example.demo.entity.User; 
+ import org.springframework.beans.factory.annotation.Autowired; 
+ import org.springframework.web.bind.annotation.*; 
+   import java.util.List; 
+   @RestController 
+ @RequestMapping("") 
+ public class UserController { 
+ @Autowired 
+ private UserMapper userMapper; 
+   @GetMapping("/user") 
+ public List<User> findAll() { 
+ return userMapper.findAll(); 
+ } 
+   @GetMapping("/user/{userId}") 
+ public User findById(@PathVariable long userId) { 
+ return userMapper.findById(userId); 
+ } 
+   @PostMapping("/user") 
+ public void insertUser(@RequestBody User user) { 
+ userMapper.insertUser(user); 
+ } 
+   @PutMapping("/user/{userId}") 
+ public void updateUser(@RequestBody User user,@PathVariable int userId) { 
+ userMapper.updateUser(userId, user.getName()); 
+ } 
+   @DeleteMapping("/user/{userId}") 
+ public void deleteById(@PathVariable long userId) { 
+ userMapper.deleteUser(userId); 
+ } 
+ } 
